484e674ec4ec11293868923c9a0def9218463b1a
Nathan Reyes <nathanreyes@me.com>
2018-09-08 20:03:22 -0500

    Modify favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ module.exports = {
       },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       {
         rel: 'stylesheet',
         href:
